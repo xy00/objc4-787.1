760c8348ae3248a53c4501938786e78e3897389b
xy00 <ios.xy00@gmail.com>
2020-09-15 10:45:15 +0800

    objc4-787.1

---
+++ objc4-787.1/debug_objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  debug_objc
+//
+//  Created by xy00 on 2020/9/15.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
